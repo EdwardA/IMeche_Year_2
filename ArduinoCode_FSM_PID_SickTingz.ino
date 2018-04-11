3c1fba61450e43b6451b2e7fd50325af8474b8fb
Edward <ea890@live.mdx.ac.uk>
2018-04-11 11:39:32 +0100

    Replaced sensor library with seperate function
    Sensor Library causes leonardo chip to freeze up, only replacing bootlaoder seemed to recover it. Replacing library with seperate function seems to fix issue

--- ArduinoCode_FSM_PID_SickTingz.ino
+++ ArduinoCode_FSM_PID_SickTingz.ino
@@ -1,34 +1,64 @@
 //=== \/ Initilise \/ ===========================
-// CHANGE PIN NUMBERS! THESE ARE PLACEHOLDERS
-int topPlateSwitchesPin = 1;
-int goPin = 2;
-int LiDARPin = 3;
-int LiDARSensorValue = 0;
+
+#include <PID_v1.h>
+//#include <TFMini.h>
+//TFMini tfmini;
+
+const int topPlateSwitchesPin = 8; // switches at top of device
+const int goPin = 6; // switch at base of robot
 int state = 0;
 int numOfMoves = 0;
-int setPoint = 220;
+double setPoint = 220;
 int timeAtStartOfPause = 0;
 int timeCheckedInPause = 0;
 bool pauseComplete = false;
+const double Kp = 5, Ki = 0, Kd = 0;
+double motorSpeed;
+double distanceReading = 0;
+unsigned int t1;
+unsigned int t2;
+PID myPID(&distanceReading, &motorSpeed, &setPoint, Kp, Ki, Kd, DIRECT);
 
+const int motorADirection1 = 5;
+const int motorADirection2 = 3;
+const int motorAEnable = 2;
+
+const int motorBDirection1 = 13;
+const int motorBDirection2 = 11;
+const int motorBEnable = 12;000
 //=== /\ initilise /\ ===========================
 //=== \/ setUp \/ ===============================
 
 void setup() {
-  for (int i = 2; i < 14; i++) {
-    pinMode(i, OUTPUT);
-  }
-  pinMode(goPin, INPUT_PULLUP);
-  pinMode(topPlateSwitchesPin, INPUT_PULLUP);
-}
+  Serial.begin(9600);
+  Serial1.begin(115200);
+  pinMode(goPin, INPUT);
+  pinMode(topPlateSwitchesPin, INPUT);
+  pinMode(goPin, INPUT);
+  pinMode(topPlateSwitchesPin, INPUT);
 
+  pinMode(motorADirection1, OUTPUT);
+  pinMode(motorADirection2, OUTPUT);
+  pinMode(motorAEnable, OUTPUT);
+
+  pinMode(motorBDirection1, OUTPUT);
+  pinMode(motorBDirection2, OUTPUT);
+  pinMode(motorBEnable, OUTPUT);
+
+  myPID.SetSampleTime(200);
+  myPID.SetMode(AUTOMATIC);
+}
 
 //=== /\ setUp /\ ==============================
 //=== \/ stateMachine \/ =======================
 
 void loop() {
-  if (state == 0) { // Ready State
-    if (goPin == LOW) {
+  Serial.print("State at interation: ");
+  Serial.println(state);
+  Serial.print("setPoint at iteration: ");
+  Serial.println(setPoint);
+  if (state == 0){ // Ready State
+    if (digitalRead(goPin) == LOW) {
       state = 1;
     }
     movement();
@@ -78,30 +108,50 @@ void loop() {
       state = 2;
     }
   }
+  delay(500);
 }
 
 //=== /\ stateMachine /\ =======================
 //=== \/ functions \/ ==========================
-// MOVEMENT NEEDS TO BE WRITEN! FUNCTION HAS A DEFINITON OF PURPOSE:
 
 void movement() {
-  /*  move with PID to the setpoint.
-      select direction based on value from sensor and setpoint given.
-      (setPoint is a global variable (the reason I asked about efficiency))
-      do not worry about stopping conditions here (see movementComplete())
-  */
+  distanceReading = double(readSensor());
+  myPID.Compute();
+  digitalWrite(motorAEnable, HIGH);//enables motor A
+  digitalWrite(motorBEnable, HIGH);//enables motor B
+  if (motorSpeed > 0) {
+    analogWrite(motorADirection1, abs(motorSpeed)); //Drives motor A forward at rate of Output
+    digitalWrite(motorADirection2, LOW);
+    analogWrite(motorBDirection1, abs(motorSpeed)); //Drives motor B forward at rate of Output
+    digitalWrite(motorBDirection2, LOW);
+  } else if (motorSpeed < 0) {
+    digitalWrite(motorADirection1, LOW);
+    analogWrite(motorADirection2, abs(motorSpeed)); //Drives motor A backwards at rate of Output
+    digitalWrite(motorBDirection1, LOW);
+    analogWrite(motorBDirection2, abs(motorSpeed)); //Drives motor B forward at rate of Output
+  } else {
+    analogWrite(motorADirection1, HIGH); //drives wheels in both directions
+    digitalWrite(motorADirection2, HIGH);//drives wheels in both directions
+    analogWrite(motorBDirection1, HIGH); //drives wheels in both directions
+    digitalWrite(motorBDirection2, HIGH);//drives wheels in both directions
+  }
+  Serial.print("distanceReading: ");
+  Serial.println(distanceReading);
+  Serial.print("PID Output: ");
+  Serial.println(motorSpeed);
+  Serial.println("");
 }
 
 bool movementComplete() {
 SafetyLoop:
   if (setPoint == 0) {
-    if (topPlateSwitchesPin == LOW) {
+    if (digitalRead(topPlateSwitchesPin) == LOW) {
       return true;
     } else {
       return false;
     }
   } else if ((setPoint == 110) || (setPoint == 220)) {
-    if (LiDARSensorValue = setPoint) {
+    if (distanceReading == setPoint) {
       return true;
     } else {
       return false;
@@ -111,4 +161,18 @@ SafetyLoop:
     goto SafetyLoop;
   }
 }
+
+int readSensor(){
+  while (Serial1.available() >= 9){
+    if ((0x59 == Serial1.read()) && (0x59 == Serial1.read())){ //Byte1 & Byte2
+      t1 = Serial1.read(); //Byte3
+      t2 = Serial1.read(); //Byte4
+      t2 <<= 8;
+      t2 += t1;
+    }
+  }
+  return t1;
+}
+
+
 
